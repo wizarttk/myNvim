6d4c4d247386148fc4be7f111ed6c65e25edc023
dadad <adadada@dadad.com>
2024-03-15 17:30:18 +0800

    在keymaps.lua中添加代码，使得Ctrl+d/u移动时将光标置于屏幕中间

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -6,3 +6,7 @@
 vim.api.nvim_set_keymap("", "<S-l>", "$", { noremap = true })
 -- 在任何模式下将 Shift+l 映射成 $
 vim.api.nvim_set_keymap("", "<S-h>", "0", { noremap = true })
+
+-- 在普通模式下，使用Ctrl + u ，Ctrl + u移动后，将光标置于屏幕中间
+vim.keymap.set("n", "<C-d>", "<C-d>zz", { desc = "Center cursor after moving down half-page" })
+vim.keymap.set("n", "<C-u>", "<C-u>zz", { desc = "Center cursor after moving down up-page" })
--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -10,7 +10,7 @@ require("lazy").setup({
   spec = {
     -- add LazyVim and import its plugins
     { "LazyVim/LazyVim", import = "lazyvim.plugins" },
-    -- add Scheme
+    -- add Scheme 自己加的
     { import = "../plugins/scheme/." },
     -- import any extras modules here
     -- { import = "lazyvim.plugins.extras.lang.typescript" },
